f3b1402db5dbde2c0c7439409958f14b0a2bf1b9
SteVen Batten <stbatt@microsoft.com>
2022-06-20 13:37:03 -0700

    vsc++ dev tools

---
+++ run_once_after_install-vs-build-tools.ps1
@@ -0,0 +1,13 @@
+# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+    $CommandLine = "-NoExit -File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+    Start-Process -FilePath pwsh.exe -Verb Runas -ArgumentList $CommandLine
+    Exit
+  }
+}
+
+# Install Visual Studio Desktop Development Workload
+& 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe' modify --quiet --norestart --wait `
+    --add "Microsoft.VisualStudio.Workload.VCTools" `
+    --installPath 'C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools'
