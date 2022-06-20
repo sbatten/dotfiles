dc289b59eafcf0a29bd100ed47d08df33578977a
SteVen Batten <stbatt@microsoft.com>
2022-06-20 12:25:02 -0700

    fonts and terminal setup

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,15 +1,15 @@
-Import-Module oh-my-posh
-Set-PoshPrompt -Theme marcduiker
-Enable-PoshTransientPrompt
-
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadLineOption -PredictionViewStyle ListView
 
 Import-Module -Name Terminal-Icons
 
 Set-Alias -Name ci -Value code-insiders.cmd
+Set-Alias -Name chezmoi -Value ~/bin/chezmoi.exe
 
 nvs use auto | Out-Null
 nvs auto on | Out-Null
 
 Import-Module z
+
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\marcduiker.omp.json" | Invoke-Expression
+Enable-PoshTransientPrompt
---
+++ run_once_before_install-packages.ps1
@@ -0,0 +1,25 @@
+# Install Keyboard Layout
+.\.local\share\chezmoi\usdhkint\usdhkint_amd64.msi /qn
+
+# Install winget Packages
+winget import --accept-source-agreements --accept-package-agreements $home\.local\share\chezmoi\winget.json
+
+# Refresh Path
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+# Install PowerShell Modules
+pwsh -NoLogo -NoProfile -Command {
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    Install-Module -Name Terminal-Icons -AcceptLicense
+    Install-Module -Name z -AcceptLicense
+}
+
+# Setup Node/Yarn
+nvs add lts
+nvs use lts
+npm install --location=global yarn
+
+# Install Font
+Invoke-WebRequest -Uri "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaCode.zip" -OutFile ~/Downloads/CascadiaCode.zip
+Expand-Archive -Path ~/Downloads/CascadiaCode.zip -DestinationPath ~/Downloads/CascadiaCode
+& "~\Downloads\CascadiaCode\Caskaydia Cove Nerd Font Complete Mono Windows Compatible.ttf"
