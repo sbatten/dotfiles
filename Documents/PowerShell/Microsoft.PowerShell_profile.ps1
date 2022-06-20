81f85430a17035fffda7748bbd6ecef1b96a0747
SteVen Batten <6561887+sbatten@users.noreply.github.com>
2022-06-20 11:12:39 -0700

    upate profile

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,15 @@
-nvs use lts
+Import-Module oh-my-posh
+Set-PoshPrompt -Theme marcduiker
+Enable-PoshTransientPrompt
+
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+Import-Module -Name Terminal-Icons
+
+Set-Alias -Name ci -Value code-insiders.cmd
+
+nvs use auto | Out-Null
+nvs auto on | Out-Null
+
+Import-Module z
