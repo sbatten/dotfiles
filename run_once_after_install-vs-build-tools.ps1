e8350d9de27b1cbc819b9cd325b831c63bda88bb
SteVen Batten <stbatt@microsoft.com>
2022-06-26 22:16:31 -0700

    use --passive

--- run_once_after_install-vs-build-tools.ps1
+++ run_once_after_install-vs-build-tools.ps1
@@ -8,7 +8,7 @@ if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 }
 
 # Install Visual Studio Desktop Development Workload
-& 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe' modify --quiet --norestart `
+& 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe' modify --passive --norestart `
     --add "Microsoft.VisualStudio.Workload.VCTools" `
     --add "Microsoft.VisualStudio.Component.VC.Tools.x86.x64" `
     --add "Microsoft.VisualStudio.Component.Windows10SDK.19041" `
