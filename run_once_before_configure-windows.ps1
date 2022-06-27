6e471740aa1572029bb488937af4976c1bd06b8c
SteVen Batten <stbatt@microsoft.com>
2022-06-26 22:11:13 -0700

    updates

---
+++ run_once_before_configure-windows.ps1
@@ -0,0 +1,11 @@
+# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+    $CommandLine = "-NoExit -File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+    Start-Process -FilePath pwsh.exe -Verb Runas -ArgumentList $CommandLine
+    Exit
+  }
+}
+
+# Windows Defender
+Set-MpPreference -ExclusionPath $($HOME)
--- run_once_before_install-packages.ps1
+++ run_once_before_install-packages.ps1
@@ -16,6 +16,7 @@ pwsh -NoLogo -NoProfile -Command {
 
 # Setup Node/Yarn
 nvs add lts
+nvs use lts
 nvs link lts
 npm install --location=global yarn
 
