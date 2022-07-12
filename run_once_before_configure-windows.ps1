4d371ac4c42a3c14286ebb1e34749c56f54a2d5a
SteVen Batten <stbatt@microsoft.com>
2022-07-12 14:32:09 -0700

    use powershell

--- run_once_before_configure-windows.ps1
+++ run_once_before_configure-windows.ps1
@@ -2,7 +2,7 @@
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
   if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
     $CommandLine = "-NoExit -File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-    Start-Process -FilePath pwsh.exe -Verb Runas -ArgumentList $CommandLine
+    Start-Process -FilePath powershell.exe -Verb Runas -ArgumentList $CommandLine
     Exit
   }
 }
